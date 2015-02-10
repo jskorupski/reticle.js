dfc0ccef6ae4fd3684d000ff10948dc615055123
James Skorupski <jskorupski@ebay.com>
2015-02-10 15:34:44 -0800

    change to double quotes for strings for easier embedding into HTML file

--- reticle.js
+++ reticle.js
@@ -1,7 +1,7 @@
 javascript:(function(){
 	if (!($ = window.jQuery)) {
-		script = document.createElement( 'script' );
-		script.src = 'https://code.jquery.com/jquery-2.1.3.min.js'; 
+		script = document.createElement( "script" );
+		script.src = "https://code.jquery.com/jquery-2.1.3.min.js"; 
 		script.onload=injectReticle;
 		document.body.appendChild(script);
 	} 
@@ -17,58 +17,58 @@ javascript:(function(){
 		var docHeight = undefined;
 		var docWidth = undefined;
 
-		$(document).on('mousemove', function(evt){
+		$(document).on("mousemove", function(evt){
 			
 			if(!reticleobjectleft) {
 				
 				docHeight = $(document).height();
 				docWidth = $(document).width();
 
-				reticleoverlay = $('<div/>', {
-					id: 'reticleoverlay', 
+				reticleoverlay = $("<div/>", {
+					id: "reticleoverlay", 
 				})
 				.height(docHeight)
 				.css({
-					'opacity' : 0.75,
-					'position': 'absolute',
-					'top': 0,
-					'left': 0,
-					'width': '100%',
-					'z-index': 5000,
-					'overflow': 'visible',
-					'pointer-events': 'none' 
+					"opacity" : 0.75,
+					"position": "absolute",
+					"top": 0,
+					"left": 0,
+					"width": "100%",
+					"z-index": 5000,
+					"overflow": "visible",
+					"pointer-events": "none" 
 				  });
-				  reticleoverlay.appendTo('body');
+				  reticleoverlay.appendTo("body");
 
 
 
 				/* Left and Top crosshair lines */
-				reticleobjectleft = $('<div/>', {
-					id: 'reticleleft',    
+				reticleobjectleft = $("<div/>", {
+					id: "reticleleft",    
 
 				})
 				.css({
-					'position': 'absolute',
-					'width': (docWidth) + 'px',
-					'height': (docHeight) + 'px',
-					'border-right': '1px solid #d00',
-					'border-bottom' : '1px solid #d00'
+					"position": "absolute",
+					"width": (docWidth) + "px",
+					"height": (docHeight) + "px",
+					"border-right": "1px solid #d00",
+					"border-bottom" : "1px solid #d00"
 				});
 
 				reticleobjectleft.appendTo(reticleoverlay);
 					
 
 				/* Right and bottom crosshair lines */
-				reticleobjectright = $('<div/>', {
-				   id: 'reticleright',    
+				reticleobjectright = $("<div/>", {
+				   id: "reticleright",    
 
 				})
 				.css({
-					'position': 'absolute',
-					'width': (docWidth) + 'px',
-					'height': (docHeight) + 'px',
-					'border-left': '1px solid #d00',
-					'border-top' : '1px solid #d00'
+					"position": "absolute",
+					"width": (docWidth) + "px",
+					"height": (docHeight) + "px",
+					"border-left": "1px solid #d00",
+					"border-top" : "1px solid #d00"
 				});
 					
 				reticleobjectright.appendTo(reticleoverlay);
@@ -76,11 +76,11 @@ javascript:(function(){
 			}
 
 			  
-			reticleobjectleft.css('left', (evt.pageX - docWidth) + 'px');
-			reticleobjectleft.css('top', (evt.pageY - docHeight) + 'px');
+			reticleobjectleft.css("left", (evt.pageX - docWidth) + "px");
+			reticleobjectleft.css("top", (evt.pageY - docHeight) + "px");
 			
-			reticleobjectright.css('left', (evt.pageX) + 'px');
-			reticleobjectright.css('top', (evt.pageY) + 'px');
+			reticleobjectright.css("left", (evt.pageX) + "px");
+			reticleobjectright.css("top", (evt.pageY) + "px");
 		   
 		});
 	}
